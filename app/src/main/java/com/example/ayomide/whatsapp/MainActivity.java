6fd3cd5d0d49b048f2f0844feec8ac495b3c3adf
SKPnation <ayomideseaz@gmail.com>
2018-12-29 05:38:20 +0000

    added toolbar with title

--- app/src/main/java/com/example/ayomide/whatsapp/MainActivity.java
+++ app/src/main/java/com/example/ayomide/whatsapp/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.ayomide.whatsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private android.support.v7.widget.Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("WhatsApp");
     }
 }
