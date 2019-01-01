6bc87781f4a137d762b6625650d3fd62e3c67d70
SKPnation <ayomideseaz@gmail.com>
2019-01-01 20:43:04 +0000

    Authentication with Firebase: User can now create an account.

--- app/src/main/java/com/example/ayomide/whatsapp/RegisterActivity.java
+++ app/src/main/java/com/example/ayomide/whatsapp/RegisterActivity.java
@@ -1,12 +1,20 @@
 package com.example.ayomide.whatsapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 import org.w3c.dom.Text;
@@ -17,11 +25,20 @@ public class RegisterActivity extends AppCompatActivity
     private Button CreateAccountButton;
     private MaterialEditText UserEmail, UserPassword;
     private TextView AlreadyHaveAccountLink;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog loadingBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_register );
+
+
+        mAuth = FirebaseAuth.getInstance();
 
         InitializeFields();
 
@@ -32,6 +49,14 @@ public class RegisterActivity extends AppCompatActivity
                 SendUserToLoginActivity();
             }
         });
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                CreateNewAccount();
+            }
+        });
     }
 
     private void InitializeFields()
@@ -40,12 +65,50 @@ public class RegisterActivity extends AppCompatActivity
         UserEmail = findViewById( R.id.register_email );
         UserPassword = findViewById( R.id.register_password );
         AlreadyHaveAccountLink = findViewById( R.id.already_have_account_link );
+
+        loadingBar = new ProgressDialog(this);
     }
 
-    private void SendUserToMainActivity()
+    private void CreateNewAccount()
     {
-        Intent registerIntent = new Intent(RegisterActivity .this, MainActivity.class);
-        startActivity(registerIntent);
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
+        }
+        else
+            {
+                loadingBar.setTitle("Creating New Account");
+                loadingBar.setMessage("Please wait, while we are creating a new account for you...");
+                loadingBar.setCanceledOnTouchOutside(true);
+                loadingBar.show();
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task)
+                            {
+                                if(task.isSuccessful())
+                                {
+                                    SendUserToLoginActivity();
+                                    Toast.makeText(RegisterActivity.this, "Account created successfully", Toast.LENGTH_SHORT).show();
+                                    loadingBar.dismiss();
+                                }
+                                else
+                                    {
+                                        String message = task.getException().toString();
+                                        Toast.makeText( RegisterActivity.this, "Error : " + message, Toast.LENGTH_SHORT ).show();
+                                        loadingBar.dismiss();
+                                    }
+                            }
+                        });
+            }
     }
 
     private void SendUserToLoginActivity()
