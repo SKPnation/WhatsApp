1038535fe6acbcd70680654797f283716cc056ab
SKPnation <ayomideseaz@gmail.com>
2019-01-01 19:46:07 +0000

    Initialized fields.

--- app/src/main/java/com/example/ayomide/whatsapp/LoginActivity.java
+++ app/src/main/java/com/example/ayomide/whatsapp/LoginActivity.java
@@ -3,21 +3,48 @@ package com.example.ayomide.whatsapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class LoginActivity extends AppCompatActivity
 {
 
     FirebaseUser currentUser;
+    private Button LoginButton, PhoneLoginButton;
+    private MaterialEditText UserEmail, UserPassword;
+    private TextView DontHaveAccountLink, ForgotPasswordLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
+        
+        InitializeFields();
+
+        DontHaveAccountLink.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                SendUserToRegisterActivity();
+            }
+        });
     }
 
+    private void InitializeFields()
+    {
+        LoginButton = findViewById( R.id.login_button );
+        PhoneLoginButton = findViewById( R.id.phone_login_button );
+        UserEmail = findViewById( R.id.login_email );
+        UserPassword = findViewById( R.id.login_password );
+        DontHaveAccountLink = findViewById( R.id.dont_have_account_link );
+        ForgotPasswordLink = findViewById( R.id.forgot_password_link );
+    }
 
 
     @Override
@@ -36,7 +63,13 @@ public class LoginActivity extends AppCompatActivity
 
     private void SendUserToMainActivity()
     {
-        Intent mainIntent = new Intent(LoginActivity  .this, MainActivity.class);
-        startActivity(mainIntent);
+        Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(loginIntent);
+    }
+
+    private void SendUserToRegisterActivity()
+    {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
--- app/src/main/java/com/example/ayomide/whatsapp/RegisterActivity.java
+++ app/src/main/java/com/example/ayomide/whatsapp/RegisterActivity.java
@@ -1,13 +1,56 @@
 package com.example.ayomide.whatsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.rengwuxian.materialedittext.MaterialEditText;
 
+import org.w3c.dom.Text;
+
+public class RegisterActivity extends AppCompatActivity
+{
+
+    private Button CreateAccountButton;
+    private MaterialEditText UserEmail, UserPassword;
+    private TextView AlreadyHaveAccountLink;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_register );
+
+        InitializeFields();
+
+        AlreadyHaveAccountLink.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                SendUserToLoginActivity();
+            }
+        });
+    }
+
+    private void InitializeFields()
+    {
+        CreateAccountButton = findViewById( R.id.register_button );
+        UserEmail = findViewById( R.id.register_email );
+        UserPassword = findViewById( R.id.register_password );
+        AlreadyHaveAccountLink = findViewById( R.id.already_have_account_link );
+    }
+
+    private void SendUserToMainActivity()
+    {
+        Intent registerIntent = new Intent(RegisterActivity .this, MainActivity.class);
+        startActivity(registerIntent);
+    }
+
+    private void SendUserToLoginActivity()
+    {
+        Intent loginIntent = new Intent(RegisterActivity .this, LoginActivity .class);
+        startActivity(loginIntent);
     }
 }
