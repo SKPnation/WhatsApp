5229af17ab54fb0004f2b2684e4d699fa8be43cd
SKPnation <ayomideseaz@gmail.com>
2019-01-02 10:24:38 +0000

    added profile_image using batch drawable importe because SettingsActivity kept on crashing due to copy and paste method of the default profile image.

--- app/src/main/java/com/example/ayomide/whatsapp/SettingsActivity.java
+++ app/src/main/java/com/example/ayomide/whatsapp/SettingsActivity.java
@@ -2,35 +2,14 @@ package com.example.ayomide.whatsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity
 {
 
-    private Button UpdateAccountSettings;
-    private EditText Username, UserStatus;
-    private CircleImageView UserProfileImage;
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_settings );
 
-
-        InitializeFields();
-    }
-
-    private void InitializeFields()
-    {
-
-        UpdateAccountSettings = findViewById( R.id.update_settings_button );
-        Username = findViewById( R.id.set_user_name );
-        UserStatus = findViewById( R.id.set_profile_status );
-        UserProfileImage = findViewById( R.id.set_profile_image );
     }
 }
